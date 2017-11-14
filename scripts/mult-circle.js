ad8c358dd0ceae3f581d41ec26b537ac20299df8
Connor Couetil <couetilc@gmail.com>
2017-11-14 11:36:58 -0500

    1) Displays multiplier values for all positive floating point numbers 2) Displays performance metrics related to draw time for a frame 3) Clean up structure of the functions and control flow

--- scripts/mult-circle.js
+++ scripts/mult-circle.js
@@ -5,15 +5,30 @@ var ctx = canvas.getContext("2d");
 //Record canvas variables used for calculations
 var x_origin = canvas.width / 2;
 var y_origin = canvas.height / 2;
-var radius_offset = 5;
-var radius = Math.min(x_origin, y_origin) - radius_offset;
+var radius_offset = 5; //distance of radius to edge of canvas
+var label_offset = 0; //distance of label to corresponding point
+var radius = Math.min(x_origin, y_origin) - radius_offset - label_offset;
+var Lines = new Object(); //Stores location of all lines during computation
 
 //Visual variables
 var point_color = "red";
 var circle_color = "black";
 var line_color = "black";
+var text_color = "black";
 var point_size = 3;
-var clean_canvas;
+var text_size = 8;
+var clean_canvas; //visual state of canvas with no lines drawn
+//Possibilities for coloring lines:
+//  a) Each M increment applies a single value on color spectrum to all lines
+//  b) Each line drawn is a different color along spectrum. (more psychedelic)
+//      -> May want to change alpha value so the colors blend on intersection
+
+//Performance tracking variables
+var last_drawtime = 0;
+var avg_drawtime = 0;
+var num_draws = 0;
+var div_lastdraw = document.getElementById("last_drawtime");
+var div_avgdraw = document.getElementById("avg_drawtime");
 
 
 //Set parameters for algorithm:
@@ -22,11 +37,13 @@ var clean_canvas;
     //circle with a period of n.
 
 // set period (modulus parameter) of the algorithm
-var N = 200;
-// initialize coefficient M
-var M = 2;
-// store location of points
-points = {}
+var N;
+var nInput = document.getElementById("n-value");
+nInput.addEventListener('input', updateCanvas);
+// initialize coefficient M (the multiplier)
+var M;
+var mInput = document.getElementById("m-value");
+mInput.addEventListener('input', updateTimesTable);
 
 function drawCircle() {
     ctx.beginPath();
@@ -44,8 +61,8 @@ function drawNPointsOnCircle(n) {
         var ratio = -angle * Math.PI / 180;
         var x = x_origin + radius * Math.cos(ratio);
         var y = y_origin + radius * Math.sin(ratio);
-        points[i] = {'x':x, 'y':y};
 
+        //Draw point
         ctx.beginPath();
         ctx.arc(x, y, point_size, 0, 2 * Math.PI);
         ctx.fillStyle = point_color;
@@ -54,39 +71,95 @@ function drawNPointsOnCircle(n) {
     ctx.closePath();
 }
 
+function drawCanvas() {
+    N = nInput.value;
+    drawCircle();
+    drawNPointsOnCircle(N);
+    clean_canvas = ctx.getImageData(0,0,canvas.width,canvas.height);
+}
+
+function calcTimesTable() {
+    var val_x, val_fx, angle, ratio;
+    for (i = 0; i < N; i++) {
+        //determine domain (argument) & range (value) for the modular 
+        //multiplication function
+        val_x = i;
+        val_fx = M * val_x % N;
+
+        //determine argument and value locations on the canvas
+        angle = 180 + 360 * val_x / N;
+        ratio = -angle * Math.PI / 180;
+        x_dim1 = x_origin + radius * Math.cos(ratio);
+        x_dim2 = y_origin + radius * Math.sin(ratio);
+
+        angle = 180 + 360 * val_fx / N;
+        ratio = -angle * Math.PI / 180;
+        fx_dim1 = x_origin + radius * Math.cos(ratio);
+        fx_dim2 = y_origin + radius * Math.sin(ratio);
+
+        //store line components to draw later
+        Lines[val_x] = { x:{'dim1':x_dim1, 'dim2':x_dim2},
+                        fx:{'dim1':fx_dim1,'dim2':fx_dim2}};
+    }
+}
+
 function drawTimesTable() {
-    var input, output;
+    //calculate point values for all lines to be drawn
+    calcTimesTable();
+
+    //draw all lines
     ctx.strokeStyle = line_color;
     for (i = 0; i < N; i++) {
-        //determine domain/range value for the modular multiplication function
-        input = i;
-        output = M * input % N;
-
-        //draw the line representing the map/function for the given value
         ctx.beginPath();
-        ctx.moveTo(points[input].x, points[input].y);
-        ctx.lineTo(points[output].x, points[output].y);
+        ctx.moveTo(Lines[i].x['dim1'], Lines[i].x['dim2']);
+        ctx.lineTo(Lines[i].fx['dim1'], Lines[i].fx['dim2']);
         ctx.stroke();
     }
     ctx.closePath();
 }
 
-function drawCanvas() {
-    drawCircle();
-    drawNPointsOnCircle(N);
-    clean_canvas = ctx.getImageData(0,0,canvas.width,canvas.height);
-    //Remember to take advantage of x-axis symettry at some point to cut
-    //down on number of computations (For both Points and Lines)
-    drawTimesTable();
-}
+function updateTimesTable() {
+    //measure performance
+    num_draws = num_draws + 1;
+    var t0 = performance.now();
 
-function updateParameters() {
+    //remove all currently drawn lines from canvas
     ctx.putImageData(clean_canvas, 0, 0);
+
+    //store multiplier value supplied by user
     M = mInput.value;
+
+    //update canvas by drawing the new times table
     drawTimesTable();
+
+    //record time taken to draw
+    var t1 = performance.now();
+    last_drawtime = t1 - t0;
+    avg_drawtime = avg_drawtime + (last_drawtime - avg_drawtime) / num_draws;
+
+    //display performance metrics
+    updatePerformance();
 }
 
-drawCanvas();
+function updatePerformance() {
+    div_lastdraw.innerHTML = last_drawtime.toFixed(4);
+    div_avgdraw.innerHTML = avg_drawtime.toFixed(4);
+}
 
-mInput = document.getElementById("m-value");
-mInput.addEventListener('change', updateParameters);
+function updateCanvas() {
+    //clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //clear performance metrics
+    num_draws = 0;
+    last_drawtime = 0;
+    avg_drawtime = 0;
+
+    //redraw canvas using new parameters
+    drawCanvas();
+
+    //redraw lines using new parameters
+    updateTimesTable();
+}
+
+updateCanvas();
